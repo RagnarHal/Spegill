367cbd2ebae4958cddae6d2ec77e8b4428af9d6a
Ragnar <ragnarmh13@ru.is>
2016-01-06 00:15:09 +0000

    Refactoring in python code; Discarded redundant getCalendar function and moved it to the events controller itself. Minor project structure cleanup. Now fetches and displays current weather (wip)

--- mirror.py
+++ mirror.py
@@ -1,37 +1,43 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, jsonify, request, abort, make_response
-from icalendar import Calendar, Event
-from datetime import datetime, date, time, timedelta
+import flask
+import icalendar
+import datetime
 import requests
+import json
 
-app = Flask(__name__, template_folder='views')
+app = flask.Flask(__name__, template_folder='views')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 # Returns today's events in JSON format as a list of events
-@app.route('/getevents')
-def getEvents():
-	url = request.args.get('url')
+@app.route('/events')
+def get_events():
+	# Get the URL parameter from the client and attempt to GET it.
+	url = flask.request.args.get('url')
+	# TODO: Error logging
 	try:
-		calendar = getCalendar(url)
-	except ValueError as e:
-		abort(400, e.message)
-	except requests.HTTPError as e:
-		abort(400, e.message)
+		response = requests.get(url)
+	except requests.exceptions.MissingSchema:
+		flask.abort(400, "No URL schema supplied. Perhaps you meant http://" + url + "?")
+	# Attempt to parse the result into an icalendar object.
+	try:
+		calendar = icalendar.Calendar.from_ical(response.content)
+	except ValueError:
+		flask.abort(400, "Not a valid ical calendar.")
 
 	events = []
 	for event in calendar.walk('VEVENT'):
 		# dtstart.dt will return a date object and not a datetime object if the event is a full-day event.
 		# Since a date object does not have a date() method, calling date() on an all-day event will fail.
-		start_day = event['dtstart'].dt.date() if isinstance(event['dtstart'].dt, datetime) else event['dtstart'].dt
-		end_day = event['dtend'].dt.date() if isinstance(event['dtend'].dt, datetime) else event['dtend'].dt
-		start_time = event['dtstart'].dt.time() if isinstance(event['dtstart'].dt, datetime) else ""
-		end_time = event['dtend'].dt.time() if isinstance(event['dtend'].dt, datetime) else ""
+		start_day = event['dtstart'].dt.date() if isinstance(event['dtstart'].dt, datetime.datetime) else event['dtstart'].dt
+		end_day = event['dtend'].dt.date() if isinstance(event['dtend'].dt, datetime.datetime) else event['dtend'].dt
+		start_time = event['dtstart'].dt.time() if isinstance(event['dtstart'].dt, datetime.datetime) else ""
+		end_time = event['dtend'].dt.time() if isinstance(event['dtend'].dt, datetime.datetime) else ""
 
-		today = date.today()
+		today = datetime.date.today()
 
 		if today <= start_day:
 			events.append({	'summary' : event['summary'],
@@ -43,31 +49,27 @@ def getEvents():
 							'end_time' : str(end_time),
 							'is_today' : 1 if start_day == today else 0})
 
-	return jsonify(results=events)
+	return flask.jsonify(results=events)
+
+@app.route('/weather')
+def get_weather():
+	#url = flask.request.args.get('url')
+	#response = requests.get(url)
+	# For now, just return the json string straight up to the client.
+	# Later, maybe filter out useless information
+	d = '{"coord":{"lon":-21.86,"lat":64.12},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],"base":"stations","main":{"temp":273.15,"pressure":996,"humidity":85,"temp_min":273.15,"temp_max":273.15},"visibility":10000,"wind":{"speed":7.2,"deg":70},"clouds":{"all":0},"dt":1452029400,"sys":{"type":1,"id":4832,"message":0.0316,"country":"IS","sunrise":1451992382,"sunset":1452009206},"id":6692263,"name":"Reykjavik","cod":200}'
+	
+	return flask.jsonify(results=json.loads(d))
 
 @app.errorhandler(404)
 def not_found(e):
-	resp = make_response(e.description, 404)
+	resp = flask.make_response(e.description, 404)
 	return resp
 
 @app.errorhandler(500)
 def server_error(e):
-	resp = make_response(e.description, 500)
+	resp = flask.make_response(e.description, 500)
 	return resp
-
-# Fetches calendar from the given url and returns it as an icalendar object
-def getCalendar(url):
-	try:
-		response = requests.get(url)
-	except requests.HTTPError:
-		raise requests.HTTPError("No URL schema supplied. Perhaps you meant http://" + url + "?")
-
-	try:
-		calendar = Calendar.from_ical(response.content)
-	except ValueError:
-		raise ValueError("Not a valid ical calendar.")
-
-	return calendar
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,29 @@
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 $( document ).ready(function($) {
 	console.log("Document ready");
 
 	$(".widget-calendar-error").hide();
+	$(".widget-weather-error").hide();
 	// Initialize the event list. Will go into its own init function along with more functionality
-	update_calendar();
 
 	// Debug button for updating the calendar instantly when needed
 	$("#update-cal").click(function() {
 		update_calendar();
 	});
+	$("#update-weather").click(function() {
+		update_weather();
+	})
 
 	init_clock();
-
-	update_interval = 60*60*1000;
-	setInterval(function() {
-		update_calendar();
-	}, update_interval);
+	//init_calendar(60*60*1000)
 });
+
+function init_calendar(interval) {
+	setInterval(function() {
+			update_calendar();
+		}, interval);
+}
 
 function init_clock() {
 	setInterval(function() {
@@ -25,20 +32,54 @@ function init_clock() {
 	}, 1000);
 }
 
+function update_weather() {
+	// Correct URL:
+	// Today:
+	// http://api.openweathermap.org/data/2.5/weather?id=6692263&appid=2de143494c0b295cca9337e1e96b00e0
+	// Forecast:
+	// http://api.openweathermap.org/data/2.5/forecast?id=6692263&appid=2de143494c0b295cca9337e1e96b00e0
+	var url = 'http://api.openweathermap.org/data/2.5/weather?id=6692263&appid=2de143494c0b295cca9337e1e96b00e0'
+
+	$.get("http://127.0.0.1:5000/weather", {url : url}, function(data) {
+		console.log("Got response from Weather API");
+		$(".widget-weather-error").hide();
+		var weather = data.results;
+		console.log(data.results);
+		if(weather.length == 0) $(".widget-weather").text("No weather :(");
+		console.log(weather.dt);
+		var date = new Date(weather.dt*1000)
+		console.log(date);
+		
+		$("#weather-calc-date").text(monthNames[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear());
+		$("#weather-calc-time").text(('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2));
+		$("#weather-temp").text(weather.main.temp - 273.15 + "°C");
+		$("#weather-main").text(weather.weather.main);
+		$("#weather-desc").text(weather.weather[0].description);
+		$("#weather-windspeed").text(weather.wind.speed + " m/s");
+		$("#weather-winddirection").text(weather.wind.deg + "°");
+		$("#weather-cloudpercent").text(weather.clouds.all + "% clouds");
+	})
+	.fail(function(data) {
+		var msg = $(data.responseText).filter("p").html();
+		$(".widget-weather-error").text("Request for weather went wrong. Error code: " + data.status + ": " + msg);
+		$(".widget-weather-error").show();
+	})
+}
+
 function update_calendar() {
 	// Corrent URL:
 	// https://calendar.google.com/calendar/ical/ragnarhal%40gmail.com/private-3aaff115768f7c84e5b9ab1d4655344c/basic.ics
 	var url = "https://calendar.google.com/calendar/ical/ragnarhal%40gmail.com/private-3aaff115768f7c84e5b9ab1d4655344c/basic.ics";
 	// Get the calendar from the server
-	$.get("http://127.0.0.1:5000/getevents", {url : url}, function(data) {
-		console.log("Success");
+	$.get("http://127.0.0.1:5000/events", {url : url}, function(data) {
+		console.log("Got response from Calendar API");
 		$(".widget-calendar-error").hide();
 		$(".widget-calendar").empty();
 		var calendar = data.results;
 		if(calendar.length == 0) $(".widget-calendar").text('No upcoming events!');
 		calendar.sort(compare_events);
 		for (var e in calendar) {
-			console.log(calendar[e].summary);
+			//console.log(calendar[e].summary);
 			var today = (calendar[e].is_today == 1 ? true : false)
 			// TODO: Find a better way of building the markup
 			$(".widget-calendar").append('<div class="list-group event">\
